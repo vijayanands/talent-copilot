5bb68947faf974a64b89dfde02f0c6a29396c9bf
Vijay Sankarasubramanian <vijayanands@gmail.com>
2024-08-17 09:27:26 -0700

    fixed some more checks in change password

--- main.py
+++ main.py
@@ -467,6 +467,8 @@ def account_page():
             st.session_state.change_password_match_error = ""
         if 'current_password_error' not in st.session_state:
             st.session_state.current_password_error = ""
+        if 'new_password_error' not in st.session_state:
+            st.session_state.new_password_error = ""
 
         current_password = st.text_input("Current Password", type="password", key="current_password")
         new_password = st.text_input("New Password", type="password", key="new_password",
@@ -485,6 +487,10 @@ def account_page():
         if st.session_state.current_password_error:
             st.error(st.session_state.current_password_error)
 
+        # Display new password error if it exists
+        if st.session_state.new_password_error:
+            st.error(st.session_state.new_password_error)
+
         if st.button("Change Password"):
             if not current_password or not new_password or not confirm_new_password:
                 st.error("Please fill in all password fields.")
@@ -498,6 +504,9 @@ def account_page():
                 if not verify_current_password(user.id, current_password):
                     st.session_state.current_password_error = "Current password is incorrect."
                     st.error("Current password is incorrect.")
+                elif current_password == new_password:
+                    st.session_state.new_password_error = "New password must be different from the current password."
+                    st.error("New password must be different from the current password.")
                 else:
                     if change_user_password(user.id, new_password):
                         st.success("Password changed successfully! You will now be logged out.")
