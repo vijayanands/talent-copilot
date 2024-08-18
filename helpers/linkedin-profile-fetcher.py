5544125574d0663e28d0e6d9f66b90042191cb75
Vijay Sankarasubramanian <vijayanands@gmail.com>
2024-08-18 11:41:50 -0700

    ran black and added code to fetch linkedin profile using proxycurl

---
+++ helpers/linkedin-profile-fetcher.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("PROXYCURL_API_KEY")
+headers = {"Authorization": "Bearer " + api_key}
+api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+
+
+def get_linkedin_profile_json(profile):
+    params = {
+        "linkedin_profile_url": profile,
+        "extra": "include",
+        "skills": "include",
+    }
+    response = requests.get(api_endpoint, params=params, headers=headers)
+    data = response.json()
+    keys_to_remove = ["people_also_viewed", "connections"]
+    my_dict = {k: v for k, v in data.items() if k not in keys_to_remove}
+    print(json.dumps(response.json(), indent=4))
+    return my_dict
