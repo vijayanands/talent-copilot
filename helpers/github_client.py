aeb3b52427c5194fb6ea9d00c5bcf6090e60128e
Vijay Sankarasubramanian <vijayanands@gmail.com>
2024-09-03 23:46:29 -0700

    fixed bug

--- helpers/github_client.py
+++ helpers/github_client.py
@@ -7,7 +7,7 @@ import requests
 from helpers import import_env
 
 headers = {
-    "Authorization": f"token {os.getenv("GITHUB_TOKEN")}",
+    "Authorization": f"token {os.getenv('GITHUB_TOKEN')}",
     "Accept": "application/vnd.github.v3+json",
 }
 
